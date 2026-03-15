74fba48d84fa11e5a6e5582a8cc600960eab9cc3
virzarasydann <virzarasydan@gmail.com>
2026-03-15 18:14:56 +0700

    menambahkan implementasi dengan table driven

---
+++ tpModul4_103082400034/KodePos.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpModul4_103082400034
+{
+    internal class KodePos
+    {
+        public static string getKodePos(string namaKelurahan)
+        {
+
+            string[] kelurahan = new string[]
+        {
+            "Batununggal",
+            "Kujangsari",
+            "Mengger",
+            "Wates",
+            "Cijaura",
+            "Jatisari",
+            "Margasari",
+            "Sekejati",
+            "Kebonwaru",
+            "Maleer",
+            "Samoja"
+        };
+
+            
+            string[] kodePos = new string[]
+            {
+            "40266",
+            "40287",
+            "40267",
+            "40256",
+            "40287",
+            "40286",
+            "40286",
+            "40286",
+            "40272",
+            "40274",
+            "40273"
+            };
+
+            
+            string inputBersih = namaKelurahan.Trim().ToLower();
+
+            
+            for (int i = 0; i < kelurahan.Length; i++)
+            {
+                
+                if (kelurahan[i].ToLower() == inputBersih)
+                {
+                    
+                    return $"{kelurahan[i]} {kodePos[i]}";
+                }
+            }
+
+            
+            return "Data tidak ditemukan";
+
+
+        }
+
+        public static void getKodePos()
+        {
+
+            string[] kelurahan = new string[]
+        {
+            "Batununggal",
+            "Kujangsari",
+            "Mengger",
+            "Wates",
+            "Cijaura",
+            "Jatisari",
+            "Margasari",
+            "Sekejati",
+            "Kebonwaru",
+            "Maleer",
+            "Samoja"
+        };
+
+
+            string[] kodePos = new string[]
+            {
+            "40266",
+            "40287",
+            "40267",
+            "40256",
+            "40287",
+            "40286",
+            "40286",
+            "40286",
+            "40272",
+            "40274",
+            "40273"
+            };
+
+
+            
+
+
+            for (int i = 0; i < kelurahan.Length; i++)
+            {
+
+                Console.WriteLine("Kelurahan : "+kelurahan[i]);
+                Console.WriteLine("Kode Pos : " + kodePos[i]);
+            }
+
+
+            
+
+
+        }
+    }
+
+
+    
+    
+}
--- tpModul4_103082400034/Program.cs
+++ tpModul4_103082400034/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpModul4_103082400034
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            KodePos.getKodePos();
+        }
+       
+    }
+}
